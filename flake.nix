291b542a8930b8aae03bed8c9c097883cad1971e
fennecs <coffee@fnnx.dev>
2026-07-17 00:04:16 +1000

    fix: patch

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "flake python";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default =
+      pkgs.mkShell
+      {
+        packages = [
+          (pkgs.python314.withPackages (p:
+            with p; [
+              uv
+            ]))
+        ];
+
+        env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.stdenv.cc.cc.lib
+          pkgs.libz
+        ];
+
+        shellHook = ''
+          echo "woof"
+        '';
+      };
+  };
+}
